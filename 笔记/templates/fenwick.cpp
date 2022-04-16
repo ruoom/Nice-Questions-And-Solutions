e06c6009f7ef23fe4d2858f5f7e1189378c16cfe
ruoom <44316768+ruoom@users.noreply.github.com>
2022-04-16 11:47:33 +0800

    Create fenwick.cpp

---
+++ 笔记/templates/fenwick.cpp
@@ -0,0 +1,23 @@
+
+template <typename T>
+struct Fenwick {
+    const int n;
+    std::vector<T> a;
+    Fenwick(int n) : n(n), a(n) {}
+    void add(int x, T v) {
+        for (int i = x; i < n; i += i & -i) {
+            a[i] += v;
+        }
+    }
+    T sum(int x) {
+        T ans = 0;
+        for (int i = x; i > 0; i -= i & -i) {
+            ans += a[i];
+        }
+        return ans;
+    }
+    T sum(int l, int r) {
+        return sum(r) - sum(l);
+    }
+};
+//-------------------------------------------------------------------------------------
