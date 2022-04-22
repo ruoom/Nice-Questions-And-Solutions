f98c1b93e72e24f0b9a8b174867784399bfa768e
ruoom <44316768+ruoom@users.noreply.github.com>
2022-04-22 11:02:30 +0800

    Create F.cpp

---
+++ Contest/Codeforces Round #784 (Div. 4)/unoexp/F.cpp
@@ -0,0 +1,80 @@
+///////////////////////
+// 　　　　　／＞　 フ 
+// 　　　　| 　_　 _| 
+//  　　　／`ミ _x 彡 
+// 　 　 /　　　 　 | 
+// 　　 /　  ヽ　　 ? 
+// ／￣|　　 |　|　| 
+// | (￣ヽ＿_ヽ_)_) 
+// ＼二つ 
+///////////////////////
+//#pragma GCC optimize(2)
+#include<bits/stdc++.h>
+#include<numeric>
+using namespace std;
+#define int long long
+#define ll long long
+#define ull unsigned long long
+#define endl '\n'
+#define mkp(a,b) make_pair(a,b)
+#define pii pair<int,int>
+#define umap unordered_map
+#define all(x) x.begin(),x.end()
+#define Sum(a) (accumulate (all(a), 0ll))
+#define inf 2e18
+#ifdef LOCAL
+    #include "Debug.h"
+#else
+    #define debug(...) 42
+#endif
+void ____() {   srand(time(0));
+    cin.tie(0)->sync_with_stdio(0);  //cout.tie(0);
+#ifdef LOCAL
+    #include "Debug.h"
+    bool uno = freopen("..\\in.txt", "r", stdin);
+    bool exp = freopen("..\\out.txt", "w", stdout);
+#endif
+}
+template<class T> istream& operator>>(istream& is, pair<T, T>& v) { is >> v.first >> v.second;       return is; }
+template<class T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i;        return is; }
+template<class T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' ';return os; }
+//-------------------------------------------------------------------------------------
+
+
+signed main() {
+    ____();
+
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        cin >> a;
+        int l = 0, r = n - 1;
+        int lhs = a[l], rhs = a[r];
+        int ans = 0;
+        while(l < r){
+            if (lhs == rhs) {
+                ans = l + n - r + 1;
+                l++; r--;
+                lhs += a[l];
+                rhs += a[r];
+            }
+            else if (lhs < rhs) {
+                l++;
+                lhs += a[l];
+            } else if (lhs > rhs) {
+                r--;
+                rhs += a[r];
+            }
+        }
+        cout << ans << endl;
+
+    }
+
+
+
+    return 0;
+}
