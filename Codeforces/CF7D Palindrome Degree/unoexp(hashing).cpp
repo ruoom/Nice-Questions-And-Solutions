8c3410f77d5b37c950d87c3a93bcbc07cfd778b5
ruoom <44316768+ruoom@users.noreply.github.com>
2022-05-02 15:13:23 +0800

    Create unoexp(hashing).cpp

---
+++ Codeforces/CF7D Palindrome Degree/unoexp(hashing).cpp
@@ -0,0 +1,70 @@
+///////////////////////
+// 　　　　　／＞　 フ 
+// 　　　　| 　_　 _| 
+//  　　　／`ミ _x 彡 
+// 　 　 /　　　 　 | 
+// 　　 /　  ヽ　　 ? 
+// ／￣|　　 |　|　| 
+// | (￣ヽ＿_ヽ_)_) 
+// ＼二つ 
+///////////////////////
+//#pragma GCC optimize(2)
+#include<bits/stdc++.h>
+#include<numeric>
+using namespace std;
+#define int long long
+#define ll long long
+#define ull unsigned long long
+#define endl '\n'
+#define mkp(a,b) make_pair(a,b)
+#define pii pair<int,int>
+#define umap unordered_map
+#define all(x) x.begin(),x.end()
+#define Sum(a) (accumulate (all(a), 0ll))
+#define inf 2e18
+#ifdef LOCAL
+    #include "Debug.h"
+#else
+    #define debug(...) 42
+#endif
+void ____() {   srand(time(0));
+    cin.tie(0)->sync_with_stdio(0);  //cout.tie(0);
+#ifdef LOCAL
+    #include "Debug.h"
+    bool uno = freopen("..\\in.txt", "r", stdin);
+    bool exp = freopen("..\\out.txt", "w", stdout);
+#endif
+}
+template<class T> istream& operator>>(istream& is, pair<T, T>& v) { is >> v.first >> v.second;       return is; }
+template<class T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i;        return is; }
+template<class T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' ';return os; }
+//-------------------------------------------------------------------------------------
+
+const int mod = 998244353;
+
+
+
+signed main() {
+    ____();
+
+    string s;
+    cin >> s;
+
+    int n = s.size();
+    vector<int> ans(n + 1);
+    int pref = 0, suff = 0;
+    int p = 1;
+    for (int i = 1; i <= n; ++i) {
+        pref = pref * 10007 + s[i - 1];
+        suff = suff + p * s[i - 1];
+        p *= 10007;
+        if (pref == suff) {
+            ans[i] = ans[i / 2] + 1;
+        }
+    }
+    cout << Sum(ans) << endl;
+
+
+
+    return 0;
+}
