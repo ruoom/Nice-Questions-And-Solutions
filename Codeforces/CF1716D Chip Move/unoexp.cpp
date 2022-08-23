4680424adf1650875d3be6ff831df5e7b1784f3b
unoexp <44316768+ruoom@users.noreply.github.com>
2022-08-23 18:55:05 +0800

    Create unoexp.cpp

---
+++ Codeforces/CF1716D Chip Move/unoexp.cpp
@@ -0,0 +1,136 @@
+#pragma GCC optimize(2)
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll long long
+#define ull unsigned long long
+#define endl '\n'
+#define mkp(a,b) make_pair(a,b)
+#define pii pair<int,int> 
+#define umap unordered_map
+#define all(x) x.begin(),x.end()
+#define Sum(a) (accumulate (all(a), 0ll))
+#define inf 2e18
+#ifdef LOCAL
+#include "Debug.h"
+#else
+#define debug(...) 42
+#endif
+void ____() {
+    srand(time(0));
+    cin.tie(0)->sync_with_stdio(0);
+#ifdef LOCAL
+    bool uno = freopen("..\\in.txt", "r", stdin);
+    bool exp = freopen("..\\out.txt", "w", stdout);
+#endif
+}
+template<class T> istream& operator>>(istream& is, pair<T, T>& v) { is >> v.first >> v.second;       return is; }
+template<class T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i;        return is; }
+template<class T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << endl; return os; }
+//-------------------------------------------------------------------------------------
+
+const int N = 200005;
+const int mod = 998244353;
+
+int norm(int x) {
+    if (x < 0) {
+        x += mod;
+    }
+    if (x >= mod) {
+        x -= mod;
+    }
+    return x;
+}
+template<class T>
+T power(T a, int b) {
+    T res = 1;
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
+    }
+    return res;
+}
+struct Z {
+    int x;
+    Z(int x = 0) : x(norm(x)) {}
+    int val() const {
+        return x;
+    }
+    Z operator-() const {
+        return Z(norm(mod - x));
+    }
+    Z inv() const {
+        assert(x != 0);
+        return power(*this, mod - 2);
+    }
+    Z &operator*=(const Z &rhs) {
+        x = int64_t(x) * rhs.x % mod;
+        return *this;
+    }
+    Z &operator+=(const Z &rhs) {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    Z &operator-=(const Z &rhs) {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    Z &operator/=(const Z &rhs) {
+        return *this *= rhs.inv();
+    }
+    friend Z operator*(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend Z operator+(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend Z operator-(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend Z operator/(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res /= rhs;
+        return res;
+    }
+};
+//-------------------------------------------------------------------------------------
+
+Z f[N][2];
+Z ans[N];
+Z ff[N];
+
+signed main() {
+    ____();
+
+    int n, c;
+    cin >> n >> c;
+
+    f[0][0] = 1;
+
+    for (int i = 1; i < 655; ++i) {
+        if (i > 1) f[0][0] = 0;
+        int k = c + i - 1;
+        for (int j = 1; j <= n ; ++j) {
+            ff[j] = 0;
+            if (j >= k) {
+                ff[j] = f[j - k][!(i & 1)] + ff[j - k];
+            }
+            f[j][i & 1] = ff[j];
+            ans[j] += ff[j];
+        }
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        cout << ans[i].val() << " ";
+    }
+
+        
+    return 0;
+}
