1374b726b55ee43f4bcbd04d94a8c1c705876544
unoexp <44316768+ruoom@users.noreply.github.com>
2022-09-10 12:48:58 +0800

    Update math.cpp

--- 笔记/templates/math.cpp
+++ 笔记/templates/math.cpp
@@ -8,6 +8,14 @@ template<class T> inline T qpowqm(T a, T b, T mod) { T res = 1; a %= mod; while 
 template<class T> inline T qpow(T a, T b, T mod) { T res = 1; a %= mod; while (b != 0) { if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1; }return res; }
 template<class T> inline T qpow(T a, T b) { T res = 1; while (b != 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; }return res; }
 template<class T> inline T gcd(T __m, T __n) { T __t; while (__n != 0) { __t = __m % __n; __m = __n; __n = __t; }return __m; }
-template<class T> void Exgcd(T a, T b, T& x, T& y) { if (b == 0) x = 1, y = 0; else Exgcd(b, a % b, y, x), y -= a / b * x; }
+template<class T> int Exgcd(T a, T b, T& x, T& y) { if (b == 0) { x = 1, y = 0; return a; } else { int d = Exgcd(b, a % b, y, x); y -= a / b * x; return d; } }
 const int dx[4]{ 1,0,-1,0 }, dy[4]{ 0,1,0,-1 }; // for every grid problem!!
 //-------------------------------------------------------------------------------------
+
+std::array<int, 3> exgcd(int a, int b) {
+    if (b == 0) {
+        return {a, 1, 0};
+    }
+    auto [g, x, y] = exgcd(b, a % b);
+    return {g, y, x - a / b * y};
+}
